08614506b619f3e1979291aae0319df119a580c5
ThiagoVictoriano <thiagovic03@gmail.com>
2022-05-13 13:55:49 -0300

    Pequenos detalhes

--- EP2 jogo.py
+++ EP2 jogo.py
@@ -3926,7 +3926,7 @@ while tentativas < 20:
       if dica == '5':
         continente = dados_normalizados[pais_escolhido]['continente']
         tentativas+=7
-        print('o continente do país é {}'.format(continente))
+        print('o continente do país é a/o {}'.format(continente))
     if escolha == 'pais':
       guess = input('Digite o nome de um país: ')
       while guess not in dados_normalizados:
